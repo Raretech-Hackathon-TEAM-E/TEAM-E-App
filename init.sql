6d92990e07e221ffdb177a52f7b8d64a38d490d0
kentarousui <kentarousui1985920@gmail.com>
2022-11-09 22:35:38 +0900

    テストユーザを追記

--- init.sql
+++ init.sql
@@ -30,3 +30,6 @@ CREATE TABLE messages (
     m_add_time timestamp NOT NULL default current_timestamp,
     quote_mid bigint unsigned UNIQUE NULL
 );
+
+/* ユーザーネーム：テスト　パスワード：testpass */
+INSERT INTO users(uid, user_name, email, password, u_signup_time) VALUES('7bf934b7-8a22-4bfd-832b-fe68fb29c76f', 'テスト', 'test@gmail.com', '13d249f2cb4127b40cfa757866850278793f814ded3c587fe5889e889a7a9f6c', '2022-11-08 0:20:20');
