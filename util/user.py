a1a849161bba91e57467df142819f1b2ac8a90e3
Mai Miz <m.711029@gmail.com>
2022-11-24 08:01:33 +0900

    サインアップでエラーが起きる問題を解消（user.pyをいじったせいでした）

--- util/user.py
+++ util/user.py
@@ -1,7 +1,7 @@
 class User:
-    def __init__(self, uid, user_name, email, password):
+    def __init__(self, uid, name, email, password):
         self.uid = uid
-        self.user_name = user_name
+        self.name = name
         self.email = email
         self.password = password
     
