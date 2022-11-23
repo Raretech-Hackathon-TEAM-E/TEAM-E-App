9d4eaaf6413e2cd8e20c8931278ed841a2b7d6c7
Mai Miz <m.711029@gmail.com>
2022-11-23 17:13:51 +0900

    ページネーション実装

--- static/js/addmodal.js
+++ static/js/addmodal.js
@@ -6,14 +6,15 @@ const addChannelBtn = document.getElementById("add-channel-btn");
 console.log(addChannelBtn);
 // 追加モーダルクローズボタン取得
 const buttonClose = document.getElementById("add-page-close-btn");
+
+/*
+
 // チャンネル削除モーダル取得
-/* 削除機能いったんコメントアウト
 const deleteChannelModal = document.getElementById("delete-channel-modal");
 // チャンネル削除モーダルを開くボタン取得
 const deleteChannelBtn = document.getElementById("delete-channel-btn");
 // 削除モーダルクローズボタン取得
 const deleteClose = document.getElementById("delete-page-close-btn");
-
 */
 
 // モーダルを開く
