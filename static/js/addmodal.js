9767e3363ed2dc04969a53e1186f80a7314e2d20
Mai Miz <m.711029@gmail.com>
2022-11-23 11:41:15 +0900

    チャンネル追加モーダル表示確認

--- static/js/addmodal.js
+++ static/js/addmodal.js
@@ -1,23 +1,28 @@
-const Modal = document.querySelector(".modal");
-const addChannelBtn = document.querySelector("#add-channel-btn");
-const buttonClose = document.querySelector(".modalClose");
-const deleteChannelBtn = document.querySelector(".delete-channel-btn");
+// チャンネル追加モーダル取得
+const addChannelModal = document.getElementById("add-channel-modal");
+console.log(addChannelModal);
+// チャンネル追加モーダルを開くボタン取得
+const addChannelBtn = document.getElementById("add-channel-btn");
+console.log(addChannelBtn);
+// 追加モーダルクローズボタン取得
+const buttonClose = document.getElementById("add-page-close-btn");
+
 
 // モーダルを開く
 // <button id="add-channel-btn">チャンネル追加</button>ボタンがクリックされた時
-addChannelBtn.addEventListener('click', modalOpen);
+addChannelBtn.addEventListener("click", modalOpen);
 function modalOpen() {
-  modal.style.display = 'block';
+  addChannelModal.style.display = "block";
 }
 
-buttonClose.addEventListener('click', modalClose);
+buttonClose.addEventListener("click", modalClose);
 function modalClose() {
-  modal.style.display = 'none';
+  addChannelModal.style.display = "none";
 }
 
-addEventListener('click', outsideClose);
+addEventListener("click", outsideClose);
 function outsideClose(e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
+  if (e.target == addChannelModal) {
+    addChannelModal.style.display = "none";
   }
 }
