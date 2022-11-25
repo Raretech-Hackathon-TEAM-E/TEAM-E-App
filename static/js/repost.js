74a2e931c502c7cbc8d72e752bfb0543e5d88f6a
Mai Miz <m.711029@gmail.com>
2022-11-25 15:24:26 +0900

    レイアウトの崩れを修正

--- static/js/repost.js
+++ static/js/repost.js
@@ -1,4 +1,68 @@
 
+//repost
+const repostModal = document.getElementById("repost-message-modal");
+// モーダルを開くボタン取得
+const repostBtn = document.getElementById("repost-button");
+// モーダルクローズボタン取得
+const repostPageButtonClose = document.getElementById("repost-page-close-btn");
+
+
+
+const repostMessage = () => {
+  if (uid !== message.uid) {
+    return;
+  } else {
+    modalOpen("repost");
+  }
+};
+
+repostBtn.addEventListener("click", () => {
+  modalOpen("repost");
+});
+
+function modalOpen(mode) {
+  if (mode === "repost") {
+    repostModal.style.display = "block";
+  }
+}
+
+// モーダル内のバツ印がクリックされた時
+
+repostPageButtonClose.addEventListener("click", () => {
+  modalClose("repost")
+});
+
+function modalClose(mode) {
+  if (mode === "repost") {
+    repostModal.style.display = "none";
+  }
+}
+
+/*
+// モーダルを開く
+// <button id="add-channel-btn">チャンネル追加</button>ボタンがクリックされた時
+addChannelBtn.addEventListener("click", modalOpen);
+function modalOpen() {
+  addChannelModal.style.display = "block";
+}
+
+
+deleteChannelBtn.addEventListener("click", modalOpen);
+function modalOpen() {
+  deleteChannelModal.style.display = "block";
+}
+*/
+
+
+// モーダルコンテンツ以外がクリックされた時
+addEventListener("click", outsideClose);
+function outsideClose(e) {
+  if (e.target == repostModal) {
+    repostModal.style.display = "none";
+  }
+}
+
+/*
 function repostsubmit() {
     if(uid !== item.uid) {
         return
@@ -10,3 +74,4 @@ function repostsubmit() {
       repostButton.setAttribute("type", submit);
   };
 }
+*/
