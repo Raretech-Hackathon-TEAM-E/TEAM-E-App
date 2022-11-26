155b6e349bd38c4b9b9c48a13290072926cb1a44
Mai Miz <m.711029@gmail.com>
2022-11-26 16:00:22 +0900

    最終確認

--- app.py
+++ app.py
@@ -283,6 +283,19 @@ def logout():
     return redirect('/login')
 
 
+
+# 404エラー
+@app.errorhandler(404)
+def show_error404(error):
+    return render_template('error/404.html')
+
+
+#500エラー
+@app.errorhandler(500)
+def show_error500(error):
+    return render_template('error/500.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
